e899fd5f62279c3375564f9aedabaa143a1bd767
Administrator <bward6700@gmail.com>
2022-07-26 10:25:00 -0400

    updates

---
+++ src/Board.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Square from './Square'
+
+const Board = () => {
+  return (
+    <Square value = "hello world"/>
+  )
+}
+
+export default Board
---
+++ src/Square.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Square = () => {
+    return (
+        <button className='button'>
+            {this.props.value}
+
+        </button>
+    )
+}
+
+export default Square
